8f90dec07252ee46e668dcad9a12075d99bbf24d
MityaAndreevich <dmitry.logachev@icloud.com>
2022-04-05 20:56:45 +0300

    add textField to ToDoVC  programmatically

--- ToDoApp(coreData)/ToDoViewController.swift
+++ ToDoApp(coreData)/ToDoViewController.swift
@@ -8,8 +8,29 @@
 import UIKit
 
 class ToDoViewController: UIViewController {
+    private lazy var textField: UITextField = {
+       let textField = UITextField()
+        textField.placeholder = "New Task"
+        textField.borderStyle = .roundedRect
+        
+        return textField
+    }()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         view.backgroundColor = .white
+        setupSubViews(textField)
+        setConstraints()
+    }
+    
+    private func setupSubViews(_ subview: UIView...) {
+        subview.forEach { subview in
+            view.addSubview(subview)
+        }
+    }
+    
+    private func setConstraints() {
+        textField.translatesAutoresizingMaskIntoConstraints = false
+        
     }
 }
