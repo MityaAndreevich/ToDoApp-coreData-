b9edd9ae2bdbe916206be49c8e26177c67e530da
MityaAndreevich <dmitry.logachev@icloud.com>
2022-04-05 12:14:12 +0300

    prepare app setup navBar

---
+++ ToDoApp(coreData)/ToDoListViewController.swift
@@ -0,0 +1,39 @@
+//
+//  ViewController.swift
+//  ToDoApp(coreData)
+//
+//  Created by Dmitry Logachev on 04.04.2022.
+//
+
+import UIKit
+
+class ToDoListViewController: UITableViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .white
+        setupNavigationBar()
+    }
+    
+    private func setupNavigationBar() {
+        title = "ToDo List"
+        navigationController?.navigationBar.prefersLargeTitles = true
+        
+        let navBarAppearance = UINavigationBarAppearance()
+        navBarAppearance.backgroundColor = UIColor(
+            red: 21/255,
+            green: 101/255,
+            blue: 192/255,
+            alpha: 194/255
+        )
+        
+        navBarAppearance.titleTextAttributes = [.foregroundColor: UIColor.white]
+        navBarAppearance.largeTitleTextAttributes = [.foregroundColor: UIColor.white]
+        
+        navigationController?.navigationBar.standardAppearance = navBarAppearance
+        navigationController?.navigationBar.scrollEdgeAppearance = navBarAppearance
+    }
+
+
+}
+
