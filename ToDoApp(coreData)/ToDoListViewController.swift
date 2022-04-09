c8c8feec4a9db2431252eaee7e975ba7fb856652
MityaAndreevich <dmitry.logachev@icloud.com>
2022-04-09 20:35:02 +0300

    add alertController add save method in alert

--- ToDoApp(coreData)/ToDoListViewController.swift
+++ ToDoApp(coreData)/ToDoListViewController.swift
@@ -52,9 +52,7 @@ class ToDoListViewController: UITableViewController {
     }
     
     @objc private func addNewTask() {
-        let todoVC = ToDoViewController()
-        todoVC.delegate = self
-        present(todoVC, animated: true)
+        showAlert(with: "New Task", and: "What do You want to do?")
     }
     
     private func fetchData() {
@@ -64,6 +62,40 @@ class ToDoListViewController: UITableViewController {
             todoList = try context.fetch(fetchRequest)
         } catch let error {
             print("Failed to fetch data", error)
+        }
+    }
+    
+    private func showAlert(with title: String, and message: String) {
+        let alert = UIAlertController(title: title, message: message, preferredStyle: .alert)
+        let saveAction = UIAlertAction(title: "Save", style: .default) { _ in
+            guard let task = alert.textFields?.first?.text, !task.isEmpty else { return }
+            self.save(task)
+        }
+        let cancelAction = UIAlertAction(title: "Cancel", style: .destructive)
+        alert.addAction(saveAction)
+        alert.addAction(cancelAction)
+        alert.addTextField { textField in
+            textField.placeholder = "New Task"
+        }
+        
+        present(alert, animated: true)
+    }
+    
+    private func save(_ taskName: String) {
+        guard let entityDescription = NSEntityDescription.entity(forEntityName: "Task", in: context) else { return }
+        guard let task = NSManagedObject(entity: entityDescription, insertInto: context) as? Task else { return }
+        task.taskTitle = taskName
+        todoList.append(task)
+        
+        let cellIndex = IndexPath(row: todoList.count - 1, section: 0)
+        tableView.insertRows(at: [cellIndex], with: .automatic)
+        
+        if context.hasChanges {
+            do {
+                try context.save()
+            } catch let error {
+                print(error)
+            }
         }
     }
 }
